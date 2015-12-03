d624b76b7e22c928174d3a4d2f5946330a1eced9
Kaffa-MY <meng-yang@foxmail.com>
2015-12-03 13:10:15 +0800

    modify FlockerNodeRedeploy.sh

--- FlockerNodeRedeploy.sh
+++ FlockerNodeRedeploy.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
 # Move Python code from the Git clone to where they are used
 rm -rf /opt/flocker/lib/python2.7/site-packages/flocker/
 cp -r /flocker-source/flocker/flocker/ /opt/flocker/lib/python2.7/site-packages/
 
-SYSTEMD_SOURCE_DIR=/flocker-source/flocker/admin/package-files/systemd/
+SYSTEMD_SOURCE_DIR=/etc/systemd/system/multi-user.target.wants
 SOURCE_SERVICE_FILES=$(
-   ls ${SYSTEMD_SOURCE_DIR}/*.service | 
+   ls ${SYSTEMD_SOURCE_DIR}/*.service |
+   grep flocker |
    xargs -n 1 -I {} sh -c 'basename {} .service'
 );
 
@@ -13,12 +15,6 @@ for service in ${SOURCE_SERVICE_FILES};
 do
     systemctl stop ${service}
 done
-
-# Move systemd unit files from the clone to where systemd will look for them
-# This uses /bin/cp instead of cp because sometimes cp is aliased to cp -i
-# which requires confirmation
-# This overwrites existing files (-f)
-/bin/cp -f ${SYSTEMD_SOURCE_DIR}/* /etc/systemd/system/multi-user.target.wants
 
 # Reload systemd, so that it can find new or changed units:
 systemctl daemon-reload
