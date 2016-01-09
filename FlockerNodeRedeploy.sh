b44f51c81054d1027c3c4472c2f23c079f9f9915
Kaffa-MY <meng-yang@foxmail.com>
2016-01-09 10:24:15 +0800

    add node redeployment script

---
+++ FlockerNodeRedeploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+git checkout 1.8.0_LC
+git pull
+
+# Move Python code from the Git clone to where they are used
+rm -rf /opt/flocker/lib/python2.7/site-packages/flocker/
+cp -r /flocker-source/flocker/flocker/ /opt/flocker/lib/python2.7/site-packages/
+
+SYSTEMD_SOURCE_DIR=/etc/systemd/system/multi-user.target.wants
+SOURCE_SERVICE_FILES=$(
+   ls ${SYSTEMD_SOURCE_DIR}/*.service |
+   grep flocker |
+   xargs -n 1 -I {} sh -c 'basename {} .service'
+);
+
+# Stop systemd units before they are changed
+for service in ${SOURCE_SERVICE_FILES};
+do
+    systemctl stop ${service}
+done
+
+# Reload systemd, so that it can find new or changed units:
+systemctl daemon-reload
+
+# Start systemd units
+for service in ${SOURCE_SERVICE_FILES};
+do
+    if [ "$(systemctl is-enabled ${service})" == 'enabled' ]
+    then
+        systemctl start ${service}
+    fi
+done
